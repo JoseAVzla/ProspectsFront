c3f2c72addf5b6baabfb05edd99ffb3a12e11b02
josevalenzuela <pepe13.2.25@gmail.com>
2021-02-07 15:23:18 -0700

    Se actualizaron las rutas de las peticiones

--- src/actions/ProspectsActions.js
+++ src/actions/ProspectsActions.js
@@ -1,9 +1,13 @@
 import axios from "axios";
 
 export const getProspects = () => {
+
+  const config = {
+    headers:{'Acces-Control-Allow-Origin':'*'}
+  }
   return dispatch => {
     axios
-      .get("http://localhost:3001/", {porfa: "jala"})
+      .get("https://prospectos-api-nodejs-3lu95.ondigitalocean.app/prospects")
       .then(res => {
         dispatch({
           type: "get_prospects",
@@ -19,7 +23,7 @@ export const getProspects = () => {
 export const getProspect = (idProspect) => {
   console.log("Action " + idProspect + " Si hay");
   return dispatch => {
-    axios.get(`http://localhost:3001/find-by-id?id=${idProspect}`, {id: idProspect})
+    axios.get(`https://prospectos-api-nodejs-3lu95.ondigitalocean.app/prospects/${idProspect}`, {id: idProspect})
     .then(res => {
       console.log(res.data);
       dispatch({
@@ -34,7 +38,7 @@ export const postProspect = ( nombre, apellidop, apellidom, calle, numero, colon
   return dispatch => {
     console.log(nombre)
     axios
-      .post("http://localhost:3001/save", {
+      .post("https://prospectos-api-nodejs-3lu95.ondigitalocean.app/prospects/add", {
         nombre,
         apellidop,
         apellidom,
@@ -55,24 +59,24 @@ export const postProspect = ( nombre, apellidop, apellidom, calle, numero, colon
   };
 };
 
-export const deleteProspect = id => {
-  return dispatch => {
-    axios
-    .delete("http://localhost:3001/delete", { data: { id } })
-    .then(res => {
-      dispatch({
-        type: "delete_prospect",
-        payload: res.data
-      });
-    });
-  };
-};
+// export const deleteProspect = id => {
+//   return dispatch => {
+//     axios
+//     .delete("http://localhost:3001/delete", { data: { id } })
+//     .then(res => {
+//       dispatch({
+//         type: "delete_prospect",
+//         payload: res.data
+//       });
+//     });
+//   };
+// };
 
 export const updateProspect = (id, estatus, observacion) => {
   return dispatch => {
     axios
-      .patch("http://localhost:3001/update", { id, estatus, observacion })
-      .then(res => {
+    .patch(`https://prospectos-api-nodejs-3lu95.ondigitalocean.app/update/${id}`, { estatus, observacion })
+    .then(res => {
         dispatch({
           type: "update_prospect",
           payload: res.data
