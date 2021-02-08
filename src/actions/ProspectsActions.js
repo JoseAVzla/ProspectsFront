5ce34411c2f1e2c585c9df490600a615d5f48462
josevalenzuela <pepe13.2.25@gmail.com>
2021-02-08 12:52:28 -0700

    Agregando las peticiones a la api de DigitalOcean (NodeJs)

--- src/actions/ProspectsActions.js
+++ src/actions/ProspectsActions.js
@@ -4,7 +4,7 @@ export const getProspects = (page, rowsPerPage) => {
   console.log(page + " " + rowsPerPage)
   return dispatch => {
     axios
-      .get(`http://localhost:3001/prospects/page=${page}&limit=${rowsPerPage}`)
+      .get(`https://prospectos-api-nodejs-3lu95.ondigitalocean.app/prospects/page=${page}&limit=${rowsPerPage}`)
       .then(res => {
         dispatch({
           type: "get_prospects",
@@ -19,7 +19,7 @@ export const getProspects = (page, rowsPerPage) => {
 //https://prospectos-api-nodejs-3lu95.ondigitalocean.app/prospects
 export const getProspect = (idProspect) => {
   return dispatch => {
-    axios.get(`http://localhost:3001/prospects/${idProspect}`)
+    axios.get(`https://prospectos-api-nodejs-3lu95.ondigitalocean.app/prospects/${idProspect}`)
     .then(res => {
       dispatch({
         type: "get_prospect_by_id",
@@ -33,7 +33,7 @@ export const postProspect = ( nombre, apellidop, apellidom, calle, numero, colon
   return dispatch => {
     console.log(nombre)
     axios
-      .post("http://localhost:3001/prospects/add", {
+      .post("https://prospectos-api-nodejs-3lu95.ondigitalocean.app/add", {
         nombre,
         apellidop,
         apellidom,
@@ -71,7 +71,7 @@ export const updateProspect = (id, estatus, observacion) => {
   console.log(estatus + " " + observacion)
   return dispatch => {
     axios
-    .patch(`http://localhost:3001/update/${id}`, { estatus, observacion })
+    .patch(`https://prospectos-api-nodejs-3lu95.ondigitalocean.app/update/${id}`, { estatus, observacion })
     .then(res => {
         dispatch({
           type: "update_prospect",
